a98e3c4b4486647b3224459f886ee5f3c8d1e0f6
David Daly <ddalyjr@gmail.com>
2023-07-06 10:52:33 -0400

    add fileUrl

--- Scrumdinger/Models/ScrumStore.swift
+++ Scrumdinger/Models/ScrumStore.swift
@@ -9,5 +9,14 @@ import Foundation
 
 class ScrumStore: ObservableObject {
     @Published var scrums: [DailyScrum] = []
-    /// this is a scrum comment
+    
+    private static func fileURL() throws -> URL {
+        try FileManager.default.url(
+            for: .documentDirectory,
+            in: .userDomainMask,
+            appropriateFor: nil,
+            create: false
+        )
+        .appendingPathComponent("scrums.data")
+    }
 }
